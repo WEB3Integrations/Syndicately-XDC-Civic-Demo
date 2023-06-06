339abf155683e8d67eb50f679973db0e40ba21a4
Fielding Johnston <fielding@justfielding.com>
2023-06-05 22:48:54 -0500

    final version of contracts

--- contracts/MasterNodeFund.sol
+++ contracts/MasterNodeFund.sol
@@ -1,29 +1,24 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@identity.com/gateway-protocol-eth/contracts/Gated.sol";
 import "./MasterNodeFundCertificate.sol";
+import "@identity.com/gateway-protocol-eth/contracts/Gated.sol";
 
-contract MasterNodeFund is Gated, MasterNodeFundCertificate {
-    using SafeERC20 for ERC20;
+contract MasterNodeFund is MasterNodeFundCertificate {
 
     uint256 private constant MAX_FUNDS = 10000100 * (10**18);
     uint256 private constant MIN_CONTRIBUTION = 150000 * (10**18);
     uint256 private constant MAX_CONTRIBUTION = 10000000 * (10**18);
 
-
     uint256 public currentFunds = 0;
     uint256 public nftCounter = 1;
-    uint256 public rewardsStartTime = 0; // if this needs to be private then change it before deploying, but needs to be public for testing via test contract
-    
-    uint256 private constant REWARDS_PERIOD = 2628000 seconds; // roughly a month
-    uint256 private constant LOCK_PERIOD = 94668408 seconds; // 3 years and 8 seconds
+    uint256 public rewardsStartTime = 0;
+
+    uint256 private constant REWARDS_PERIOD = 2628000 seconds; 
+    uint256 private constant LOCK_PERIOD = 94668408 seconds; 
 
     address private payoutAddress;
-    ERC20 private xdcToken;
 
     mapping(address => Investment) public investments;
     mapping(uint256 => uint256) public investmentAmounts;
@@ -34,17 +29,18 @@ contract MasterNodeFund is Gated, MasterNodeFundCertificate {
         bool claimed;
     }
 
-    constructor(address gatewayTokenContract, uint256 gatekeeperNetworkSlotId, address xdcTokenAddress) 
-        Gated(gatewayTokenContract, gatekeeperNetworkSlotId) {
-            payoutAddress = msg.sender;
-            xdcToken = ERC20(xdcTokenAddress);
+
+
+    constructor(address gatewayTokenContract, uint256 gatekeeperNetworkIndex)
+        Gated(gatewayTokenContract, gatekeeperNetworkIndex) {
+        payoutAddress = msg.sender;
     }
 
-    function contribute(uint256 amount) virtual external gated {
+    function contribute() external payable gated {
+        uint256 amount = msg.value;
+
         require(currentFunds + amount <= MAX_FUNDS, "Total funds limit reached");
         require(amount >= MIN_CONTRIBUTION && amount <= MAX_CONTRIBUTION, "Invalid contribution amount");
-
-        xdcToken.safeTransferFrom(msg.sender, address(this), amount);
 
         if (investments[msg.sender].time == 0) {
             _createInvestment(msg.sender, amount);
@@ -53,8 +49,7 @@ contract MasterNodeFund is Gated, MasterNodeFundCertificate {
         }
 
         currentFunds += amount;
-        
-        // Update rewardsStartTime if the goal is reached and rewardsStartTime is not set already
+
         if (currentFunds >= MAX_FUNDS && rewardsStartTime == 0) {
             rewardsStartTime = block.timestamp + LOCK_PERIOD;
         }
@@ -72,22 +67,22 @@ contract MasterNodeFund is Gated, MasterNodeFundCertificate {
         investmentAmounts[tokenId] = investments[investor].amount;
     }
 
-    function distributeMonthlyRewards(uint256 totalRewardAmount) external {
+    function distributeMonthlyRewards() external payable {
         require(msg.sender == payoutAddress, "Unauthorized");
         require(block.timestamp >= rewardsStartTime, "Rewards distribution not started");
-        
-        xdcToken.safeTransferFrom(msg.sender, address(this), totalRewardAmount);
 
+        uint256 totalRewardAmount = msg.value;
         uint256 tokenId = 1;
+
         while (tokenId < nftCounter) {
             if (!_exists(tokenId)) {
                 tokenId++;
                 continue;
             }
-            address holder = ownerOf(tokenId);
+            address payable holder = payable(ownerOf(tokenId));
             uint256 holderInvestment = investmentAmounts[tokenId];
-            uint256 holderReward = (holderInvestment * totalRewardAmount) / MAX_FUNDS;
-            xdcToken.safeTransfer(holder, holderReward);
+            uint256 holderReward = (holderInvestment / MAX_FUNDS) * totalRewardAmount;
+            holder.transfer(holderReward);
             tokenId++;
         }
 
@@ -100,8 +95,8 @@ contract MasterNodeFund is Gated, MasterNodeFundCertificate {
 
         uint256 claimAmount = investments[msg.sender].amount;
         require(claimAmount > 0, "No investment found");
-        
+
         investments[msg.sender].claimed = true;
-        xdcToken.safeTransfer(msg.sender, claimAmount);
+        payable(msg.sender).transfer(claimAmount);
     }
 }
